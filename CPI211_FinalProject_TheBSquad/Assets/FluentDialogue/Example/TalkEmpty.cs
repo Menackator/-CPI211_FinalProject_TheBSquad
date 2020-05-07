fd95e99adc38d94412fb4d4e61f5718d9a5179d4
CloudWannabe <55220508+CloudWannabe@users.noreply.github.com>
2020-05-06 22:29:05 -0700

    troubleshoot

---
+++ CPI211_FinalProject_TheBSquad/Assets/FluentDialogue/Example/TalkEmpty.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fluent;
+
+public class TalkEmpty : ConversationWithImage
+{
+    public Booleans boolean;
+
+    public override FluentNode Create()
+    {
+        return
+            If(() => boolean.FancyHint == true && boolean.CookFight == true,
+                Show() *
+                Write(0, "You shouldn't see this") *
+                Options(
+                    Option("->") *
+                        Hide() *
+                        End()
+                )
+            );
+    }
+
+}
